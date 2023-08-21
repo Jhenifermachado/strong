c83bc9ba750927e203ffe7c2521ec964668ff45b
gustavorudolf <gustavorudolfifc@gmail.com>
2023-08-21 15:05:23 -0300

    trabalho

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+import CarouselComp from "@/components/CarouselComp.vue";
+</script>
+
+<template>
+  <NavBar />
+  <CarouselComp />
+
+  <div class="container text-light" id="filmes">
+    <h2>Adicionar Produtos</h2>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="columns">
+        <img
+          src="https://www.gsuplementos.com.br/tema/growth/img/pitchbar/01-ab.png"
+          alt="icon frete grátis"
+        />
+        <span>FRETE GRÁTIS</span>
+      </div>
+      <div class="columns">
+        <img
+          src="https://www.gsuplementos.com.br/tema/growth/img/pitchbar/02-ab.png"
+          alt="icon boleto"
+        />
+        <span>10% Off no Boleto</span>
+      </div>
+      <div class="columns">
+        <img
+          src="https://www.gsuplementos.com.br/tema/growth/img/pitchbar/03-ab.png"
+          alt="icon cifrão cashback"
+        />
+        <span>CASHBACK</span>
+      </div>
+      <div class="columns">
+        <img
+          src="https://www.gsuplementos.com.br/tema/growth/img/pitchbar/04-ab.png"
+          alt="icon chat nutricionista"
+        />
+        <span>Nutricionista Online</span>
+      </div>
+    </div>
+  </div>
+  <div class="row wrap">    
+    <div class="img-wrap" v-for="i in 4" :key="i">
+      <img src="@/assets/img/+.png" class="img-fluid" alt="..." width="400" />
+    </div>    
+  </div>  
+  
+</template>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-direction: row;
+  margin: 20px 0;
+  justify-content: center;
+  align-content: space-around;
+}
+
+.img-wrap {
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+}
+
+.wrap {
+  flex-wrap: wrap;
+}
+
+.columns {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light cor1">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Strong Nutrition</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarScroll"
+        aria-controls="navbarScroll"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul
+          class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+          style="--bs-scroll-height: 100px"
+        >
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"
+              >PÁGINA PRINCIPAL</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">PRODUTOS</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarScrollingDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="true"
+            >
+              OPÇÕES
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li><a class="dropdown-item" href="#filmes">PRODUTOS</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">PLANOS</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <span> <img src="https://www.gsuplementos.com.br/tema/growth/img/svgs/cart-ab.svg" alt=""> </span>
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">
+            Pesquisar
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
